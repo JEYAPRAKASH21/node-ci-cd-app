6a5a8eff7ba94fc5e7c3cd3a400088ed801f8a74
JEYAPRAKASH21 <167057989+JEYAPRAKASH21@users.noreply.github.com>
2026-04-15 13:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Node.js CI/CD App is running!');
+  res.send('Webhook working 🚀');
 });
 
 app.listen(PORT, () => {
